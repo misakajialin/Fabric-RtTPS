00c88a341ec03fe60b92f04290bcd300f1cdaaf5
misakajialin <cjl2597588144@gmail.com>
2025-01-26 14:01:24 +0800

    初提交

--- src/main/java/xyz/lisbammisakait/item/FeilongduofengItem.java
+++ src/main/java/xyz/lisbammisakait/item/FeilongduofengItem.java
@@ -1,7 +1,12 @@
 package xyz.lisbammisakait.item;
 
+
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import xyz.lisbammisakait.RelightTheThreePointStrategy;
 
 public class FeilongduofengItem extends SwordItem {
     public FeilongduofengItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
@@ -10,6 +15,15 @@ public class FeilongduofengItem extends SwordItem {
     @Override
     public boolean postHit(net.minecraft.item.ItemStack stack, net.minecraft.entity.LivingEntity target, net.minecraft.entity.LivingEntity attacker) {
         target.setFireTicks(100);
+        double motionX = attacker.getRotationVector().x;
+        double motionZ = attacker.getRotationVector().z;
+
+        // 计算击退强度，这里假设一个简单的强度值
+        double knockbackStrength = 0.4;
+
+        // 给目标添加击退效果
+        target.addVelocity(-motionX * knockbackStrength, 0.2, -motionZ * knockbackStrength);
         return super.postHit(stack, target, attacker);
     }
+
 }
